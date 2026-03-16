8012f31a7223cfb58ff86f778070c412c26a040b
Tasnim Zulkifli <tasnimzulkifli@Tasnims-MacBook-Pro.local>
2026-03-16 23:27:15 +0000

    Added rate limitting

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ from app.database import engine, Base
 from app.models import models
 from app.routers import products, auth, profile, analytics, conflicts, users
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
 Base.metadata.create_all(bind=engine)
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(
     title="Skincare & Ingredient Analysis API",
@@ -21,6 +26,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,12 +6,17 @@ from app.models.models import User
 from app.schemas.schemas import UserRegister, UserLogin, UserResponse, TokenResponse
 from app.core.security import hash_password, verify_password
 from app.core.auth import create_access_token
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from fastapi import Request
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
+limiter = Limiter(key_func=get_remote_address)
 
 
 @router.post("/register", response_model=UserResponse, status_code=201)
-def register(user: UserRegister, db: Session = Depends(get_db)):
+@limiter.limit("10/minute") # Auth endpoints: max 10 requests per minute per IP — prevents brute force attacks
+def register(request: Request, user: UserRegister, db: Session = Depends(get_db)):
     # check username not taken
     if db.query(User).filter(User.username == user.username).first():
         raise HTTPException(status_code=400, detail="Username already taken")
@@ -32,7 +37,8 @@ def register(user: UserRegister, db: Session = Depends(get_db)):
 
 
 @router.post("/login", response_model=TokenResponse)
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+@limiter.limit("10/minute") #Auth endpoints: max 10 requests per minute per IP — prevents brute force attacks
+def login(request: Request,form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.username == form_data.username).first()
     if not db_user or not verify_password(form_data.password, db_user.hashed_password):
         raise HTTPException(
